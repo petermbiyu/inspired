ae6e90969990064eecaed0509ae5277c0d608c6a
peter <petersmasha@gmail.com>
2026-02-09 00:21:10 +0300

    added more packages

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import cookieParser from "cookie-parser";
 const app = express();
 const port = process.env.PORT || 3000;
 
 // == middleware ==
-app.use(cors());
+app.use(cors({ credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
