70c9e3be03df73b32d8bf9c8564d84e7caf2cab0
peter <petersmasha@gmail.com>
2026-02-15 10:35:53 +0300

    added partials in ejs

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// es_directory
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 
 // == middleware ==
 app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(_dirname, "public")));
 
 // register view engine
 app.set("view engine", "ejs");
@@ -18,5 +26,8 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello word");
+  res.render("index");
+});
+app.get((req, res) => {
+  res.send("Oops! an error occurred");
 });
